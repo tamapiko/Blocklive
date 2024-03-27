7489ac0ec8431a77828acfa3530fa472e49b1161
tamapiko <125553567+tamapiko@users.noreply.github.com>
2024-03-27 23:29:05 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,47 +1,35 @@
-chrome.runtime.sendMessage({meta:"getUsername"},function(username){
+async function getProjects() {
+  res = await fetch("https://scratch.mit.edu/session/?blreferer", {
+    headers: {
+      "X-Requested-With": "XMLHttpRequest",
+    },
+  });
+  let json = await res.json();
+  var data = await (
+    await fetch(
+      `https://spore.us.to:4000/userProjectsScratch/${json.user.username}/`
+    )
+  ).json();
+  data.forEach(function (project) {
+    var div = document.createElement("div");
+    div.className = "project";
 
-    let alreadyAdded = {}
+    var img = document.createElement("img");
+    img.src = `https://cdn2.scratch.mit.edu/get_image/project/${project.scratchId}_480x360.png`;
 
-    function addFriendGUI(name) {
-        console.log(name)
-        if(name?.toLowerCase() in alreadyAdded) {return}
-        alreadyAdded[name.toLowerCase()] = true
+    var title = document.createElement("span");
+    title.className = "title";
+    title.textContent = project.title;
 
-        let item = document.createElement('li')
-        item.username = name
-        item.innerHTML = `${name}  <span class="x" href="page2.html">(x)</span>`;
-        item.children[0].onclick = ()=>{removeFriend(name)}
-        document.querySelector('#friends').appendChild(item)
-    }
+    div.appendChild(img);
+    div.appendChild(title);
+    document.querySelector(".projects").appendChild(div);
 
-    function addFriend(name) {
-        if(name.toLowerCase() in alreadyAdded) {return}
-        if(name.toLowerCase() == username.toLowerCase()) {return}
-        if(!name.trim()) {return}
-        if(name.includes(' ')) {return}
-        document.querySelector('#searchh').value = ''
-        addFriendGUI(name)
-        fetch(`http://spore.us.to:4000/friends/${username}/${name}`,{method:"POST"});
-    }
-
-    function removeFriend(name) {
-        delete alreadyAdded[name.toLowerCase()]
-        for(let child of document.querySelector('#friends').children) {
-            if(child.username == name) {child.remove(); break;}
-        }
-        fetch(`http://spore.us.to:4000/friends/${username}/${name}`,{method:"DELETE"});
-    }
-
-    document.querySelector('#searchh').addEventListener("keyup", function(event) {
-        if (event.keyCode === 13) {
-            addFriend(document.querySelector('#searchh').value)
-        }
+    div.addEventListener("click", function () {
+      chrome.tabs.create({
+        url: `https://scratch.mit.edu/projects/${project.scratchId}/editor`,
+      });
     });
-    document.querySelector('#submit').onclick = ()=>{addFriend(document.querySelector('#searchh').value)}
-
-
-    // populate with current friends
-    fetch(`http://spore.us.to:4000/friends/${username}`).then(res=>res.json().then(list=>list.forEach(addFriendGUI)))
-});
-
-
+  });
+}
+getProjects();
